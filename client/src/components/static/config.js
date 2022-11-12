751c559bfe068b7a22b92f0873c8b35a896aaf32
Cristian Niculae <niculaec159@gmail.com>
2022-11-12 19:19:25 +0200

    backend url change production

--- client/src/components/static/config.js
+++ client/src/components/static/config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    url: process.env.NODE_ENV === 'production' ? 'https://quiz-app-game.herokuapp.com/api' : 'http://localhost:5000/api',
-}
+	url:
+		process.env.NODE_ENV === 'production'
+			? 'https://quiz-app-034k.onrender.com/api'
+			: 'http://localhost:5000/api',
+};
